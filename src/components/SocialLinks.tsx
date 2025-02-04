337593e9ce442cc67e9090bcbd5dd6a3a5915e83
RobinJesba <jb.robinm@gmail.com>
2025-02-04 22:28:09 +0530

    Fix LinkedIn URL in SocialLinks component

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -6,7 +6,7 @@ const SocialLinks = () => {
       <a href="https://github.com/RobinJesba" target="_blank" rel="noopener noreferrer" className="transition-opacity duration-300 hover:opacity-70">
         <Github className="w-6 h-6" />
       </a>
-      <a href="www.linkedin.com/in/robin-jesba" target="_blank" rel="noopener noreferrer" className="transition-opacity duration-300 hover:opacity-70">
+      <a href="https://linkedin.com/in/robin-jesba" target="_blank" rel="noopener noreferrer" className="transition-opacity duration-300 hover:opacity-70">
         <Linkedin className="w-6 h-6" />
       </a>
     </div>
